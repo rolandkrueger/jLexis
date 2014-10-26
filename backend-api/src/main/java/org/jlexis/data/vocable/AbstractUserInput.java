fdc66a314298def7b56dca43060cac9ccdffa7e3
rkrueger <roland.krueger@oio.de>
2014-10-26 18:10:01 +0100

    rework AbstractUserInput

--- backend-api/src/main/java/org/jlexis/data/vocable/AbstractUserInput.java
+++ backend-api/src/main/java/org/jlexis/data/vocable/AbstractUserInput.java
@@ -24,39 +24,32 @@
  */
 package org.jlexis.data.vocable;
 
+import com.google.common.base.Strings;
 import org.jlexis.data.vocable.terms.*;
 import org.jlexis.data.vocable.verification.VocableVerificationData;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Map;
 
 /**
- * <p>
- * This class generically represents a user's input for one vocable in one specific language. If,
- * for example, the user entered the English translation for a vocable, the corresponding
- * {@link AbstractUserInput} object would contain that translation, an example phrase, a comment and
- * some more optional data.
- * </p>
- * <p>
- * The data itself is held in a {@link java.util.Map}. By this, a maximum flexibility is achieved since the
- * particular pieces of data belonging to this {@link AbstractUserInput} can be referenced by
- * arbitrary keys. Subclasses of {@link AbstractUserInput} can freely choose which data to store in
- * this internal data structure.
- * </p>
+ * This class generically represents a user's input for one vocable in one specific language. If, for example, the user
+ * entered the English translation for a vocable, the corresponding {@link AbstractUserInput} object would contain that
+ * translation, an example phrase, a comment and some more optional data.
+ * <p/>
+ * The data itself is held in a {@link java.util.Map}. By this, a maximum flexibility is achieved since the particular
+ * pieces of data belonging to this {@link AbstractUserInput} can be referenced by arbitrary keys. Subclasses of {@link
+ * AbstractUserInput} can freely choose which data to store in this internal data structure. They are themselves
+ * responsible for correctly managing this data.
+ * <p/>
  * TODO: describe data caching feature
  *
  * @author Roland Krueger
  */
-public abstract class AbstractUserInput implements UserInputInterface {
-    private enum DataType {COMMENT, EXAMPLE, SHORT_VERSION, HTML_VERSION}
+public abstract class AbstractUserInput implements UserInput {
 
-    ;
-
-    private static Map<String, RegisteredVocableDataKey> sRegisteredKeys;
-    private static boolean sIsSynchronizedWithDatabase;
+    private static Map<String, RegisteredVocableDataKey> REGISTERED_KEYS;
+    private static boolean IS_SYNCHRONIZED_WITH_DATABASE;
 
     /**
      * Object that represents this {@link AbstractUserInput} in the database.
@@ -64,41 +57,67 @@ public abstract class AbstractUserInput implements UserInputInterface {
     private DBO_AbstractUserInput mDatabaseObject;
 
     /**
-     * The data of this {@link AbstractUserInput}. This object assembles all the fragments
-     * that forms the language specific part of a {@link Vocable}, such as
-     * the vocabulary data of one language itself, comments, examples and extra data.
+     * The data of this {@link AbstractUserInput}. This object assembles all the fragments that forms the language
+     * specific part of a {@link Vocable}, such as the vocabulary data of one language itself, comments, examples and
+     * extra data.
      */
-    private Map<RegisteredVocableDataKey, AbstractTermData> mData;
-    private String mInputType;
-    private Map<String, WordStemTerm> mWordStems;
-    private Map<String, InflectedTerm> mInflectedTerms;
+    private Map<RegisteredVocableDataKey, AbstractTermData> data;
+    private String inputType;
+    private Map<String, WordStemTerm> wordStems;
+    private Map<String, InflectedTerm> inflectedTerms;
 
     /**
-     * Cache that holds the formatted texts for the data of this {@link AbstractUserInput}. This data
-     * consists of the short version as obtained by {@link AbstractUserInput#getShortVersion()}, the
-     * HTML-version as obtained by {@link AbstractUserInput#getHTMLVersion()} etc. The cache is cleared
-     * each time that the data of this object is changed.
+     * Cache that holds the formatted texts for the data of this {@link AbstractUserInput}. This data consists of the
+     * short version as obtained by {@link AbstractUserInput#getShortVersion()}, the HTML-version as obtained by {@link
+     * AbstractUserInput#getHTMLVersion()} etc. The cache is cleared each time that the data of this object is changed.
      */
-    private Map<DataType, String> mDataCache;
+    private Map<DataType, String> dataCache;
 
     static {
-        sRegisteredKeys = new HashMap<String, RegisteredVocableDataKey>();
-        sIsSynchronizedWithDatabase = false;
+        REGISTERED_KEYS = new HashMap<>();
+        IS_SYNCHRONIZED_WITH_DATABASE = false;
     }
 
     private AbstractUserInput() {
-        mData = new HashMap<RegisteredVocableDataKey, AbstractTermData>(8);
-        mWordStems = new HashMap<String, WordStemTerm>(1);
-        mInflectedTerms = new HashMap<String, InflectedTerm>(5);
+        data = new HashMap<>(8);
+        wordStems = new HashMap<>(1);
+        inflectedTerms = new HashMap<>(5);
     }
 
     protected AbstractUserInput(String inputType) {
         this();
-//    CheckForNull.check (inputType);
-        if (inputType.trim().equals(""))
-            throw new IllegalArgumentException("Empty input type string not accepted.");
+        if (Strings.isNullOrEmpty(inputType)) {
+            throw new IllegalArgumentException("empty input type string not accepted");
+        }
 
-        mInputType = inputType;
+        this.inputType = inputType;
+    }
+
+    /**
+     * Loads the IDs of the {@link RegisteredVocableDataKey}s from the database if the corresponding key is availabe. If
+     * the key isn't available in the database yet, store the key into the database.
+     */
+    @SuppressWarnings("unchecked")
+    public static void synchronizeRegisteredKeysWithDatabase() {
+//      TODO: refactor
+//    List<RegisteredVocableDataKey> registeredKeys =
+//      new ArrayList<RegisteredVocableDataKey>(REGISTERED_KEYS.values ());
+//    Session session = JLexisPersistenceManager.getInstance ().getSession ();
+//    Query qry = session.createQuery ("FROM RegisteredVocableDataKey");
+//    List<RegisteredVocableDataKey> keys = qry.list ();
+//
+//    for (RegisteredVocableDataKey key : keys)
+//    {
+//      registeredKeys.remove (key);
+//      REGISTERED_KEYS.put (key.getKey (), key);
+//    }
+//
+//    // persist all new keys not available in the database
+//    for (RegisteredVocableDataKey key : registeredKeys)
+//    {
+//      JLexisPersistenceManager.getInstance ().saveObject (key);
+//    }
+//    IS_SYNCHRONIZED_WITH_DATABASE = true;
     }
 
     public DBO_AbstractUserInput getDatabaseObject() {
@@ -106,12 +125,12 @@ public abstract class AbstractUserInput implements UserInputInterface {
             mDatabaseObject = new DBO_AbstractUserInput();
 
         Map<RegisteredVocableDataKey, RegularTerm> preparedData = new HashMap<RegisteredVocableDataKey, RegularTerm>();
-        for (RegisteredVocableDataKey key : mData.keySet()) {
-            preparedData.put(key, new RegularTerm(mData.get(key).getNormalizedTerm()));
+        for (RegisteredVocableDataKey key : data.keySet()) {
+            preparedData.put(key, new RegularTerm(data.get(key).getNormalizedTerm()));
         }
 
         mDatabaseObject.setData(preparedData);
-        mDatabaseObject.setInputType(mInputType);
+        mDatabaseObject.setInputType(inputType);
 
         return mDatabaseObject;
     }
@@ -121,15 +140,15 @@ public abstract class AbstractUserInput implements UserInputInterface {
             throw new NullPointerException("Argument is null.");
 
         clearCache(); // clear cache when data changes
-        mData.clear();
+        data.clear();
         Map<RegisteredVocableDataKey, RegularTerm> dboMap = databaseObj.getData();
         for (RegisteredVocableDataKey key : dboMap.keySet()) {
             AbstractTermData term = getRegisteredTermTypeForKey(key.getKey());
             term.setNormalizedTerm(dboMap.get(key).getNormalizedTerm());
-            mData.put(key, term);
+            data.put(key, term);
         }
 
-        mInputType = databaseObj.getInputType();
+        inputType = databaseObj.getInputType();
     }
 
     @Override
@@ -168,26 +187,11 @@ public abstract class AbstractUserInput implements UserInputInterface {
 
     protected abstract AbstractUserInput createNewUserInputObject();
 
-    @Deprecated
-    public void appendXMLData(Document document, Element root) {
-//    for (RegisteredVocableDataKey key : mData.keySet ())
-//    {
-//      String userData = getUserData (key.getKey ());
-//      if (userData == null || userData.equals ("")) continue;
-//      Element particle = document.createElement ("particle");
-//      particle.setAttribute ("id", key.getKey ());
-//      
-//      particle.appendChild (document.createCDATASection (userData));
-//      
-//      root.appendChild (particle);
-//    }
-    }
-
     public void addUserData(String identifier, String data) {
         if (!isKeyRegistered(identifier))
             throw new IllegalStateException(String.format("Given identifier '%s' has not been provided " +
                     "by AbstractUserInput.getUserInputIdentifiers().", identifier));
-        if (!sIsSynchronizedWithDatabase)
+        if (!IS_SYNCHRONIZED_WITH_DATABASE)
             throw new IllegalStateException("Registered keys have not yet been synchronized with database. " +
                     "Call " + AbstractUserInput.class.getName() + ".synchronizeRegisteredKeysWithDatabase() first.");
         if (data == null)
@@ -196,14 +200,14 @@ public abstract class AbstractUserInput implements UserInputInterface {
         clearCache(); // clear the cache when data changes
         AbstractTermData term = getRegisteredTermTypeForKey(identifier);
         term.setUserEnteredTerm(data);
-        mData.put(sRegisteredKeys.get(identifier), term);
+        this.data.put(REGISTERED_KEYS.get(identifier), term);
     }
 
     private AbstractTermData getRegisteredTermTypeForKey(String identifier) {
-        AbstractTermData term = mInflectedTerms.get(identifier);
+        AbstractTermData term = inflectedTerms.get(identifier);
         if (term != null) return term;
 
-        term = mWordStems.get(identifier);
+        term = wordStems.get(identifier);
         if (term != null) return term;
 
         term = new RegularTerm();
@@ -211,7 +215,7 @@ public abstract class AbstractUserInput implements UserInputInterface {
     }
 
     private boolean isKeyRegistered(String identifier) {
-        return sRegisteredKeys.containsKey(identifier);
+        return REGISTERED_KEYS.containsKey(identifier);
     }
 
     public void registerUserInputIdentifiers() {
@@ -226,14 +230,14 @@ public abstract class AbstractUserInput implements UserInputInterface {
                 continue;
             }
 
-            sRegisteredKeys.put(identifier, new RegisteredVocableDataKey(identifier));
+            REGISTERED_KEYS.put(identifier, new RegisteredVocableDataKey(identifier));
         }
     }
 
     protected abstract String[] getUserInputIdentifiers();
 
-    public boolean isTypeCorrect(UserInputInterface other) {
-        return mInputType.equals(other.getUserInputIdentifier());
+    public boolean isTypeCorrect(UserInput other) {
+        return inputType.equals(other.getUserInputIdentifier());
     }
 
     public TermDataInterface getUserData(String identifier) {
@@ -241,7 +245,7 @@ public abstract class AbstractUserInput implements UserInputInterface {
             throw new IllegalStateException(String.format("Given identifier '%s' has not been provided " +
                     "by AbstractUserInput.getUserInputIdentifiers().", identifier));
 
-        AbstractTermData result = mData.get(new RegisteredVocableDataKey(identifier));
+        AbstractTermData result = data.get(new RegisteredVocableDataKey(identifier));
         if (result == null) result = new RegularTerm();
 
         return result;
@@ -249,18 +253,18 @@ public abstract class AbstractUserInput implements UserInputInterface {
 
     public boolean isDataDefinedFor(String identifier) {
         RegisteredVocableDataKey key = new RegisteredVocableDataKey(identifier);
-        return mData.containsKey(key) && !mData.get(key).isEmpty();
+        return data.containsKey(key) && !data.get(key).isEmpty();
     }
 
     public String getUserInputIdentifier() {
-        return mInputType;
+        return inputType;
     }
 
     public void replace(AbstractUserInput userInput) {
         clearCache(); // clear the cache when data changes
-        mData.clear();
-        for (RegisteredVocableDataKey key : userInput.mData.keySet()) {
-            mData.put(key, userInput.mData.get(key));
+        data.clear();
+        for (RegisteredVocableDataKey key : userInput.data.keySet()) {
+            data.put(key, userInput.data.get(key));
         }
     }
 
@@ -269,10 +273,10 @@ public abstract class AbstractUserInput implements UserInputInterface {
             throw new IllegalArgumentException(String.format("Given argument %s has not been provided " +
                     "by AbstractUserInput.getUserInputIdentifiers()", registeredKey));
 
-        if (mWordStems.containsKey(registeredKey))
+        if (wordStems.containsKey(registeredKey))
             throw new IllegalArgumentException(String.format("The key %s has already been configured as word stem.", registeredKey));
 
-        mWordStems.put(registeredKey, new WordStemTerm());
+        wordStems.put(registeredKey, new WordStemTerm());
     }
 
     public void addWordStemChild(String governingWordStemKey, String inflectedTermKey) {
@@ -280,51 +284,24 @@ public abstract class AbstractUserInput implements UserInputInterface {
             throw new IllegalArgumentException("The word stem key has not been registered with AbstractUserInput.registerIdentifier()");
         if (!isKeyRegistered(inflectedTermKey))
             throw new IllegalArgumentException("The child key has not been registered with AbstractUserInput.registerIdentifier()");
-        if (!mWordStems.containsKey(governingWordStemKey))
+        if (!wordStems.containsKey(governingWordStemKey))
             throw new IllegalArgumentException(String.format("The key %s has not yet been configured as word stem.", governingWordStemKey));
-        if (mInflectedTerms.containsKey(inflectedTermKey))
+        if (inflectedTerms.containsKey(inflectedTermKey))
             throw new IllegalArgumentException(String.format("The key %s has already been configured as an inflected term.", inflectedTermKey));
-        mInflectedTerms.put(inflectedTermKey, new InflectedTerm(mWordStems.get(governingWordStemKey)));
-    }
-
-    /**
-     * Loads the IDs of the {@link RegisteredVocableDataKey}s from the database if the corresponding
-     * key is availabe. If the key isn't available in the database yet, store the key into the database.
-     */
-    @SuppressWarnings("unchecked")
-    public static void synchronizeRegisteredKeysWithDatabase() {
-//      TODO: refactor
-//    List<RegisteredVocableDataKey> registeredKeys =
-//      new ArrayList<RegisteredVocableDataKey>(sRegisteredKeys.values ());
-//    Session session = JLexisPersistenceManager.getInstance ().getSession ();
-//    Query qry = session.createQuery ("FROM RegisteredVocableDataKey");
-//    List<RegisteredVocableDataKey> keys = qry.list ();
-//
-//    for (RegisteredVocableDataKey key : keys)
-//    {
-//      registeredKeys.remove (key);
-//      sRegisteredKeys.put (key.getKey (), key);
-//    }
-//
-//    // persist all new keys not available in the database
-//    for (RegisteredVocableDataKey key : registeredKeys)
-//    {
-//      JLexisPersistenceManager.getInstance ().saveObject (key);
-//    }
-//    sIsSynchronizedWithDatabase = true;
+        inflectedTerms.put(inflectedTermKey, new InflectedTerm(wordStems.get(governingWordStemKey)));
     }
 
     private void clearCache() {
-        if (mDataCache == null) return;
-        mDataCache.clear();
-        mDataCache = null;
+        if (dataCache == null) return;
+        dataCache.clear();
+        dataCache = null;
     }
 
     private String getDataFromCache(DataType type) {
-        if (mDataCache == null) {
-            mDataCache = new Hashtable<DataType, String>(4);
+        if (dataCache == null) {
+            dataCache = new Hashtable<DataType, String>(4);
         }
-        if (!mDataCache.containsKey(type)) {
+        if (!dataCache.containsKey(type)) {
             String data = null;
             switch (type) {
                 case COMMENT:
@@ -340,11 +317,11 @@ public abstract class AbstractUserInput implements UserInputInterface {
                     data = getShortVersionImpl();
             }
             if (data == null) data = "";
-            mDataCache.put(type, data);
+            dataCache.put(type, data);
             return data;
         }
 
-        return mDataCache.get(type);
+        return dataCache.get(type);
     }
 
     @Override
@@ -365,4 +342,6 @@ public abstract class AbstractUserInput implements UserInputInterface {
     public String getResolvedAndPurgedUserData(String identifier) {
         return getUserData(identifier).getResolvedAndPurgedTerm();
     }
+
+    private enum DataType {COMMENT, EXAMPLE, SHORT_VERSION, HTML_VERSION}
 }
