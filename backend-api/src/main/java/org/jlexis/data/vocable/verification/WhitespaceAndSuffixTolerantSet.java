c287429c80084d39aaa65813adbb6f36062495cc
rkrueger <roland.krueger@oio.de>
2014-11-13 11:48:37 +0100

    fix copyright message

--- backend-api/src/main/java/org/jlexis/data/vocable/verification/WhitespaceAndSuffixTolerantSet.java
+++ backend-api/src/main/java/org/jlexis/data/vocable/verification/WhitespaceAndSuffixTolerantSet.java
@@ -1,15 +1,13 @@
 /*
- * Created on 30.09.2009
- * 
- * Copyright 2007-2009 Roland Krueger (www.rolandkrueger.info)
- * 
+ * Copyright 2007-2014 Roland Krueger (www.rolandkrueger.info)
+ *
  * This file is part of jLexis.
  *
  * jLexis is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * jLexis is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
